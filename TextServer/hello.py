57b5590e849b5514be642e3830f8f86f251a029b
sword4869 <1084662708@qq.com>
2022-09-22 08:26:33 +0800

    nothing important

--- TextServer/hello.py
+++ TextServer/hello.py
@@ -4,31 +4,34 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("login.html")
+    return render_template("home.html")
 
 
-@app.route('/login/', methods=['POST', 'GET'])
-def login():
+@app.route('/home/', methods=['POST', 'GET'])
+def home():
     if request.method == 'POST':
-        my_str = request.form['my_str']
-        with open('response.txt', 'w', encoding='utf-8') as fp:
-            fp.write(my_str)
-        return redirect(url_for('success', my_str='yes'))
+        ################# text processing ####################
+        my_text = request.form['my_text']
+        with open('text.txt', 'w', encoding='utf-8') as fp:
+            fp.write(my_text)
+        return redirect(url_for('success', my_text='yes'))
     else:
         pass
 
 
-@app.route('/success/<my_str>')
-def success(my_str):
-    return render_template('hello.html', my_str=my_str)
+@app.route('/success/<my_text>')
+def success(my_text):
+    return render_template('text_show.html', my_text=my_text)
 
 
 if __name__ == '__main__':
+    
     # host : 默认为127.0.0.1（localhost）。设置为'0.0.0.0'以使服务器在外部可用。虽然显示Running on http://192.168.0.2:80/ (Press CTRL+C to quit)。但是还是不行。
     #       但是居然 host='192.168.0.2'，显示也一样，就行了！
     # port 默认值为5000
     app.run(host='192.168.0.2', port=80)
     # app.run(port=80)
+
 
 ##########################
 # python Hello.py
