dd4d44567a406596659fa25fcf92df47d2b86a3d
sword4869 <1084662708@qq.com>
2022-11-27 14:00:46 +0800

    flask

--- TextServer/hello.py
+++ TextServer/hello.py
@@ -1,45 +1,52 @@
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, render_template, request, url_for
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/',methods=['GET', 'POST'])
 def index():
-    return render_template("home.html")
-
-
-@app.route('/home/', methods=['POST', 'GET'])
-def home():
     if request.method == 'POST':
-        ################# text processing ####################
         my_text = request.form['my_text']
-        with open('text.txt', 'w', encoding='utf-8') as fp:
+        with open('logs/text.txt', 'w', encoding='utf-8') as fp:
             fp.write(my_text)
-        return redirect(url_for('success', my_text='yes'))
+
+        f = request.files['my_file']
+        if f.content_length > 0:
+            f.save('logs/uploaded_file.txt')
+        return redirect('/show')
     else:
-        pass
+        return render_template("home.html")
 
-
-@app.route('/success/<my_text>')
-def success(my_text):
-    return render_template('text_show.html', my_text=my_text)
+@app.route('/show')
+def show(): # 不知道这个@app.route('/show')和def show():的要不要一致都是show
+    with open('logs/text.txt', 'r', encoding='utf-8') as fp:
+        my_text = fp.readlines()
+    # data是在text_show.html中的{{data}}的名字，随便起，只要一致
+    return render_template('text_show.html', data=my_text)
 
 
 if __name__ == '__main__':
-    
-    # host : 默认为127.0.0.1（localhost）。设置为'0.0.0.0'以使服务器在外部可用。虽然显示Running on http://192.168.0.2:8080/ (Press CTRL+C to quit)。但是还是不行。
-    #       但是居然 host='192.168.0.2'，显示也一样，就行了！
+
+    # host : 默认为127.0.0.1（localhost）
     # port 默认值为5000
-    # app.run(host='192.168.0.2', port=8080)
 
-
-
-
+    # 内部， 
     #  * Running on http://127.0.0.1:8080
-    app.run(port=8080)
+    # app.run(port=8080)
+
+
+    # 外部
+    # 设置为'0.0.0.0'或者本机的'192.168.0.2'都行，前者写起来简单
+    # app.run(host='192.168.211.225', port=8888)
+    app.run(host='0.0.0.0', port=8888)
+
+
+
 
 
 ##########################
 '''
 python hello.py
 '''
+# 确保防火墙关闭了
 ##########################
